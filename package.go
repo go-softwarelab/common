bd5d216040cbc7222c84f3d926b9ee1ac63e6cff
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2025-04-01 22:58:57 +0200

    docs: improve readme and add common package documentation

---
+++ package.go
@@ -0,0 +1,21 @@
+// Package common Utilities for Go
+//
+// Welcome to the `common` library by `go-softwarelab`! This library is designed to simplify your Go development experience by providing a set of common tools and utilities. Our goal is to turn complexity into clarity, making your everyday coding tasks easier and more efficient.
+//
+// One of the standout features of this library is its powerful set of functions that enhance the usability of Go iterators (`iter.Seq` and `iter.Seq2`). These functions are designed to make working with sequences in Go as seamless and pleasant as possible.
+//
+// ## Installation
+//
+// To install the `common` library, simply run:
+//
+// ```sh
+// go get github.com/go-softwarelab/common
+// ```
+// ## Documentation
+//
+// For more detailed documentation and examples, please visit [Go Common Utils](https://go-softwarelab.github.io/common) page.
+//
+// We hope you find the `common` library useful and that it helps you write Go code that's as clear as a summer sky!
+package common
+
+//go:generate go run -C ./tools gen_docs.go
