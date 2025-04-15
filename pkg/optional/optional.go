1aa3f9c3a25304abfd01241d4dd9bf0297968873
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2025-04-15 16:07:44 +0200

    feat: add more methods to optional package

--- pkg/optional/optional.go
+++ pkg/optional/optional.go
@@ -1,14 +1,22 @@
 package optional
 
 import (
+	"errors"
 	"fmt"
+	"iter"
 	"reflect"
+
+	"github.com/go-softwarelab/common/pkg/is"
+	"github.com/go-softwarelab/common/pkg/to"
 )
+
+const valueNotPresentErrorMessage = "value is not present"
+
+var ValueNotPresent = errors.New(valueNotPresentErrorMessage)
 
 // Elem represents an optional value.
 type Elem[E any] struct {
-	value E
-	exist bool
+	value *E
 }
 
 // Empty returns an empty optional value.
@@ -16,19 +24,54 @@ func Empty[E any]() Elem[E] {
 	return Elem[E]{}
 }
 
-// Of returns an optional value with the given value.
+// Of returns an optional with the given value.
+// If the value is a pointer, and it's nil, it returns an empty optional.
+// Otherwise, it returns non-empty optional with the given value.
 func Of[E any](v E) Elem[E] {
 	val := reflect.ValueOf(v)
 	if val.Kind() == reflect.Ptr && val.IsNil() {
 		return Elem[E]{}
 	}
 
-	return Elem[E]{value: v, exist: true}
+	return Elem[E]{value: &v}
+}
+
+// OfPtr returns an optional with the value from pointer.
+// If the pointer is nil, it returns an empty optional.
+// Otherwise, it returns non-empty optional with the value pointed to by the pointer.
+func OfPtr[E any](v *E) Elem[E] {
+	if v == nil {
+		return Elem[E]{}
+	}
+
+	return Elem[E]{value: v}
+}
+
+// OfValue returns an optional for the given value.
+// If value is zero value, it returns an empty optional.
+// Otherwise, it returns non-empty optional with the given value.
+//
+// If zero value is valid existing value for you, for example when the value is int, then prefer Of() instead.
+func OfValue[E comparable](v E) Elem[E] {
+	if is.Zero[E](v) {
+		return Elem[E]{}
+	}
+
+	return Elem[E]{value: &v}
+}
+
+// Or returns this optional if present, otherwise returns the other optional.
+func (o Elem[E]) Or(other Elem[E]) Elem[E] {
+	if o.IsPresent() {
+		return o
+	}
+
+	return other
 }
 
 // MustGet returns the value if present, otherwise panics.
 func (o Elem[E]) MustGet() E {
-	return o.MustGetf("value is not present")
+	return o.MustGetf(valueNotPresentErrorMessage)
 }
 
 // MustGetf returns the value if present, otherwise panics with a custom message.
@@ -37,20 +80,91 @@ func (o Elem[E]) MustGetf(msg string, args ...any) E {
 		panic(fmt.Sprintf(msg, args...))
 	}
 
-	return o.value
+	return *o.value
+}
+
+// OrElse returns the value if present, otherwise returns the default value.
+func (o Elem[E]) OrElse(defaultValue E) E {
+	if o.IsEmpty() {
+		return defaultValue
+	}
+
+	return *o.value
+}
+
+// OrElseGet returns the value if present, otherwise returns the default value from the function.
+func (o Elem[E]) OrElseGet(defaultValue func() E) E {
+	if o.IsEmpty() {
+		return defaultValue()
+	}
+
+	return *o.value
+}
+
+// OrError returns the value if present, otherwise returns the error.
+func (o Elem[E]) OrError(err error) (E, error) {
+	if o.IsEmpty() {
+		return to.ZeroValue[E](), err
+	}
+
+	return *o.value, nil
+}
+
+// OrErrorGet returns the value if present, otherwise returns the error from the function.
+func (o Elem[E]) OrErrorGet(err func() error) (E, error) {
+	if o.IsEmpty() {
+		return to.ZeroValue[E](), err()
+	}
+
+	return *o.value, nil
+}
+
+// IfPresent executes the function if the value is present.
+func (o Elem[E]) IfPresent(fn func(E)) {
+	if o.IsPresent() {
+		fn(*o.value)
+	}
+}
+
+// IfNotPresent executes the function if the value is not present.
+func (o Elem[E]) IfNotPresent(fn func()) {
+	if o.IsEmpty() {
+		fn()
+	}
 }
 
 // IsEmpty returns true if the value is not present.
 func (o Elem[E]) IsEmpty() bool {
-	return !o.exist
+	return o.value == nil
 }
 
 // IsPresent returns true if the value is present.
 func (o Elem[E]) IsPresent() bool {
-	return o.exist
+	return o.value != nil
 }
 
 // IsNotEmpty returns true if the value is present.
 func (o Elem[E]) IsNotEmpty() bool {
-	return o.exist
+	return o.value != nil
+}
+
+// ToSeq returns the sequence with yelded value if present, otherwise returns an empty sequence.
+func (o Elem[E]) ToSeq() iter.Seq[E] {
+	return func(yield func(E) bool) {
+		if o.IsPresent() {
+			yield(*o.value)
+		}
+	}
+}
+
+// ToSeq2 returns the iter.Seq2[E, error] with yelded value if present, otherwise yields an error.
+// Useful with usage of seqerr package.
+func (o Elem[E]) ToSeq2() iter.Seq2[E, error] {
+	return func(yield func(E, error) bool) {
+		if o.IsPresent() {
+			yield(*o.value, nil)
+		} else {
+			yield(to.ZeroValue[E](), ValueNotPresent)
+		}
+	}
 }
---
+++ pkg/optional/optional_example_test.go
@@ -0,0 +1,272 @@
+package optional_test
+
+import (
+	"fmt"
+
+	"github.com/go-softwarelab/common/pkg/optional"
+	"github.com/go-softwarelab/common/pkg/seq"
+	"github.com/go-softwarelab/common/pkg/seqerr"
+)
+
+func ExampleEmpty() {
+	opt := optional.Empty[string]()
+	fmt.Println("Is empty:", opt.IsEmpty())
+	fmt.Println("Is present:", opt.IsPresent())
+
+	// Output:
+	// Is empty: true
+	// Is present: false
+}
+
+func ExampleOf() {
+	// With a value
+	opt := optional.Of("hello")
+	fmt.Println("Value:", opt.MustGet())
+	fmt.Println("Is empty:", opt.IsEmpty())
+
+	// With a nil pointer
+	var ptr *string = nil
+	optPtr := optional.Of(ptr)
+	fmt.Println("Nil pointer optional is empty:", optPtr.IsEmpty())
+
+	// Output:
+	// Value: hello
+	// Is empty: false
+	// Nil pointer optional is empty: true
+}
+
+func ExampleOfPtr() {
+	// With a value pointer
+	value := "hello"
+	opt := optional.OfPtr(&value)
+	fmt.Println("Value:", opt.MustGet())
+
+	// With a nil pointer
+	var nilPtr *string
+	optNil := optional.OfPtr(nilPtr)
+	fmt.Println("Is empty:", optNil.IsEmpty())
+
+	// Output:
+	// Value: hello
+	// Is empty: true
+}
+
+func ExampleOfValue() {
+	// Non-zero value
+	opt := optional.OfValue(42)
+	fmt.Println("Value present:", opt.IsPresent())
+	fmt.Println("Value:", opt.MustGet())
+
+	// Zero value
+	optZero := optional.OfValue(0)
+	fmt.Println("Zero value present:", optZero.IsPresent())
+
+	// Output:
+	// Value present: true
+	// Value: 42
+	// Zero value present: false
+}
+
+func ExampleElem_Or() {
+	opt1 := optional.Of("first")
+	opt2 := optional.Of("second")
+	empty := optional.Empty[string]()
+
+	// Present optional takes precedence
+	fmt.Println("First or second:", opt1.Or(opt2).MustGet())
+	fmt.Println("Empty or second:", empty.Or(opt2).MustGet())
+
+	// Output:
+	// First or second: first
+	// Empty or second: second
+}
+
+func ExampleElem_MustGet() {
+	opt := optional.Of("hello")
+	fmt.Println("Value:", opt.MustGet())
+
+	// Note: Using MustGet on empty optional would panic
+	// empty := optional.Empty[string]()
+	// empty.MustGet() // would panic with "value is not present"
+
+	// Output:
+	// Value: hello
+}
+
+func ExampleElem_MustGetf() {
+	opt := optional.Of("hello")
+	fmt.Println("Value:", opt.MustGetf("Custom error: %s", "not found"))
+
+	// Note: Using MustGetf on empty optional would panic with custom message
+	// empty := optional.Empty[string]()
+	// empty.MustGetf("Custom error: %s", "not found") // would panic with "Custom error: not found"
+
+	// Output:
+	// Value: hello
+}
+
+func ExampleElem_OrElseGet() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	counter := 0
+	getDefault := func() string {
+		counter++
+		return fmt.Sprintf("default-%d", counter)
+	}
+
+	fmt.Println("Present value:", opt.OrElseGet(getDefault))
+	fmt.Println("Empty value:", empty.OrElseGet(getDefault))
+	fmt.Println("Empty value again:", empty.OrElseGet(getDefault))
+
+	// Output:
+	// Present value: hello
+	// Empty value: default-1
+	// Empty value again: default-2
+}
+
+func ExampleElem_OrErrorGet() {
+	opt := optional.Of(42)
+	empty := optional.Empty[int]()
+
+	errCounter := 0
+	getError := func() error {
+		errCounter++
+		return fmt.Errorf("not found-%d", errCounter)
+	}
+
+	val1, err1 := opt.OrErrorGet(getError)
+	fmt.Println("Value:", val1)
+	fmt.Println("Error:", err1)
+
+	val2, err2 := empty.OrErrorGet(getError)
+	fmt.Println("Empty value:", val2)
+	fmt.Println("Empty error:", err2)
+
+	// Output:
+	// Value: 42
+	// Error: <nil>
+	// Empty value: 0
+	// Empty error: not found-1
+}
+
+func ExampleElem_OrElse() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	fmt.Println("Present value:", opt.OrElse("default"))
+	fmt.Println("Empty value:", empty.OrElse("default"))
+
+	// Output:
+	// Present value: hello
+	// Empty value: default
+}
+
+func ExampleElem_OrError() {
+	opt := optional.Of(42)
+	empty := optional.Empty[int]()
+
+	val1, err1 := opt.OrError(fmt.Errorf("not found"))
+	fmt.Println("Value:", val1)
+	fmt.Println("Error:", err1)
+
+	val2, err2 := empty.OrError(fmt.Errorf("not found"))
+	fmt.Println("Empty value:", val2)
+	fmt.Println("Empty error:", err2 != nil)
+
+	// Output:
+	// Value: 42
+	// Error: <nil>
+	// Empty value: 0
+	// Empty error: true
+}
+
+func ExampleElem_IfPresent() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	opt.IfPresent(func(value string) {
+		fmt.Println("Value is present:", value)
+	})
+
+	empty.IfPresent(func(value string) {
+		fmt.Println("This won't be printed")
+	})
+
+	// Output:
+	// Value is present: hello
+}
+
+func ExampleElem_IfNotPresent() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	opt.IfNotPresent(func() {
+		fmt.Println("This won't be printed")
+	})
+
+	empty.IfNotPresent(func() {
+		fmt.Println("This executes when empty")
+	})
+
+	// Output:
+	// This executes when empty
+}
+
+func ExampleElem_ToSeq() {
+	opt := optional.Of("hello")
+
+	var values []string
+	seq.ForEach(opt.ToSeq(), func(value string) {
+		values = append(values, value)
+	})
+
+	fmt.Println("Values:", values)
+
+	empty := optional.Empty[string]()
+	var emptyValues []string
+	seq.ForEach(empty.ToSeq(), func(value string) {
+		emptyValues = append(emptyValues, value)
+	})
+
+	fmt.Println("Empty values length:", len(emptyValues))
+
+	// Output:
+	// Values: [hello]
+	// Empty values length: 0
+}
+
+func ExampleElem_ToSeq2() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	err := seqerr.ForEach(opt.ToSeq2(), func(value string) {
+		fmt.Printf("Value: %s\n", value)
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// With empty value
+	err = seqerr.ForEach(empty.ToSeq2(), func(value string) {
+		fmt.Printf("Unexpected value: %s\n", value)
+	})
+	if err != nil {
+		fmt.Printf("Expected error: %v\n", err)
+	}
+	// Output:
+	// Value: hello
+	// Expected error: value is not present
+}
+
+func ExampleElem_IsNotEmpty() {
+	opt := optional.Of("hello")
+	empty := optional.Empty[string]()
+
+	fmt.Println("First is not empty:", opt.IsNotEmpty())
+	fmt.Println("Second is not empty:", empty.IsNotEmpty())
+
+	// Output:
+	// First is not empty: true
+	// Second is not empty: false
+}
