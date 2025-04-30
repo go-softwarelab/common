a2f3b1b81b4fb9334442ffbd804b1e3e589dd1c0
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2025-04-30 09:18:46 +0200

    feat: improve signature is.EmptyString and is.BlankString

--- pkg/is/strings.go
+++ pkg/is/strings.go
@@ -3,21 +3,33 @@ package is
 import "strings"
 
 // EmptyString returns true if the string is empty
-func EmptyString(s string) bool {
-	return s == ""
+func EmptyString[S string | *string](s S) (isEmpty bool) {
+	switch str := (any)(s).(type) {
+	case string:
+		isEmpty = str == ""
+	case *string:
+		isEmpty = str == nil || *str == ""
+	}
+	return isEmpty
 }
 
 // BlankString returns true if the string is empty or contains only whitespace
-func BlankString(s string) bool {
-	return s == "" || strings.TrimSpace(s) == ""
+func BlankString[S string | *string](str S) (isBlank bool) {
+	switch s := (any)(str).(type) {
+	case string:
+		isBlank = s == "" || strings.TrimSpace(s) == ""
+	case *string:
+		isBlank = s == nil || *s == "" || strings.TrimSpace(*s) == ""
+	}
+	return isBlank
 }
 
 // NotEmptyString returns true if the string is not empty
-func NotEmptyString(s string) bool {
+func NotEmptyString[S string | *string](s S) bool {
 	return !EmptyString(s)
 }
 
 // NotBlankString returns true if the string is not empty or contains only whitespace
-func NotBlankString(s string) bool {
+func NotBlankString[S string | *string](s S) bool {
 	return !BlankString(s)
 }
