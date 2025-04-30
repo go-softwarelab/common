5cc700e4574bb5c24bbc25997ce23992a9f74105
Damian Orzepowski <damian.orzepowski@softwarelab.com.pl>
2025-04-30 09:49:00 +0200

    feat: add slices MapOrError and FlatMapOrError

--- pkg/slices/mapper.go
+++ pkg/slices/mapper.go
@@ -2,8 +2,9 @@ package slices
 
 // Mapper is a function that maps a value of type T to a value of type R.
 type Mapper[T any, R any] = func(T) R
+type MapperWithError[T any, R any] = func(T) (R, error)
 
-// Map returns new slice where each element is a result of applying mapper to each element of the original slice and flattening the result.
+// Map returns new slice where each element is a result of applying mapper to each element of the original slice.
 func Map[E any, R any](collection []E, mapper Mapper[E, R]) []R {
 	result := make([]R, len(collection))
 
@@ -12,6 +13,22 @@ func Map[E any, R any](collection []E, mapper Mapper[E, R]) []R {
 	}
 
 	return result
+}
+
+// MapOrError returns new slice where each element is a result of applying mapper to each element of the original slice.
+// If any of the mappers return an error, the function returns an error.
+func MapOrError[E any, R any](collection []E, mapper MapperWithError[E, R]) ([]R, error) {
+	result := make([]R, len(collection))
+
+	for i, v := range collection {
+		mapped, err := mapper(v)
+		if err != nil {
+			return nil, err
+		}
+		result[i] = mapped
+	}
+
+	return result, nil
 }
 
 // FlatMap returns new slice where each element is a result of applying mapper to each element of the original slice and flattening the result.
@@ -23,6 +40,22 @@ func FlatMap[E any, R any](collection []E, mapper Mapper[E, []R]) []R {
 	}
 
 	return result
+}
+
+// FlatMapOrError returns new slice where each element is a result of applying mapper to each element of the original slice and flattening the result.
+// If any of the mappers return an error, the function returns an error.
+func FlatMapOrError[E any, R any](collection []E, mapper MapperWithError[E, []R]) ([]R, error) {
+	result := make([]R, 0, len(collection))
+
+	for _, v := range collection {
+		mapped, err := mapper(v)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, mapped...)
+	}
+
+	return result, nil
 }
 
 // Flatten flattens a slice of slices.
